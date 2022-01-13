5c11dfb31b3853a66d9aa1ee4a3466d92948df4a
mihail.beloshapkin <misha.beloshapkin@yandex.ru>
2022-01-13 12:07:28 +0300

    changes

--- traceRoute/Program.fs
+++ traceRoute/Program.fs
@@ -1,24 +1,26 @@
 ﻿open System
 open System.Net.NetworkInformation
 
+// Get ip trace.
 let getRouteInfo (ip : string) =
     let timeout = 10000
-    let maxTtl = 30
+    let maxTtlValue = 30
     let sizeOfBuffer = 16
     let buffer : byte [] = Array.zeroCreate sizeOfBuffer
     let rnd = new Random()
     rnd.NextBytes(buffer)
     use ping = new Ping()
     let rec sub ttl data =
-        if ttl = maxTtl then data else
+        if ttl = maxTtlValue then data else
             let options = new PingOptions(ttl, true)
             let reply = ping.Send(ip, timeout, buffer, options)
             match reply.Status with
-            | IPStatus.Success -> reply.Address.ToString() :: data
-            | IPStatus.TtlExpired -> sub (ttl + 1) (reply.Address.ToString() :: data)
-            | IPStatus.TimedOut -> sub (ttl + 1) ("#########" :: data)
+            | IPStatus.Success -> (ttl, reply.Address.ToString()) :: data
+            | IPStatus.TtlExpired -> sub (ttl + 1) ((ttl, reply.Address.ToString()) :: data)
+            | IPStatus.TimedOut -> sub (ttl + 1) ((ttl, "#########") :: data)
             | _ -> failwith "Something is wrong"
     sub 1 []
+
 
 [<EntryPoint>]
 let main argv =
@@ -26,5 +28,5 @@ let main argv =
     let host = Console.ReadLine()
     let result = host |> getRouteInfo |> List.rev 
     printfn "All trace:"
-    for i in 0 .. result.Length - 1 do printf "%s\n" result.[i]
+    for i in 0 .. result.Length - 1 do printf "ttl: %A  %s\n" (result.[i] |> fst) (result.[i] |> snd)
     0
